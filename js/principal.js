1385ace3f8acf6847f6cf291b17dbe099e2bf365
MarcosFernandesF <marcosrff15@gmail.com>
2022-09-20 23:33:44 -0300

    Aula 02: Variáveis e Operadores
    - Operadores em javascript
    
    - Calculando o IMC
    
    - Condição "If"

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,34 @@
 var titulo = document.querySelector(".titulo");
-
 titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+if (peso <= 0 || peso > 1000) {
+    console.log("Peso inválido");
+    tdImc.textContent = "Peso Inválido!";
+    pesoEhValido = false;
+}
+
+if (altura <= 0 || altura > 3) {
+    console.log("Altura inválida")
+    tdImc.textContent = "Altura Inválida!";
+    alturaEhValida = false;
+}
+
+if (alturaEhValida && pesoEhValido) {
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
+
+
