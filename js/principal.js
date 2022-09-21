f89b104bb2f768dcc536e0ac2ed05d50f4096691
MarcosFernandesF <marcosrff15@gmail.com>
2022-09-21 15:49:58 -0300

    Aula 03: Arrays, Loop e Estilos
    - Como utilizar o querySelectorAll
    
    - Como alterar estilos do css com "style e classList"
    
    - Utilizando loops e arrays
    
    - Casas decimais com "to.Fixed"

--- js/principal.js
+++ js/principal.js
@@ -1,34 +1,44 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente"); // querySelectorAll traz todas as ocorrencias daquela classe
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+for (var i = 0; i < pacientes.length; i++) {
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+    var paciente = pacientes[i]; // Truque para trocar paciente por paciente[i]
 
-var tdImc = paciente.querySelector(".info-imc");
-
-var pesoEhValido = true;
-var alturaEhValida = true;
-
-if (peso <= 0 || peso > 1000) {
-    console.log("Peso inválido");
-    tdImc.textContent = "Peso Inválido!";
-    pesoEhValido = false;
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+    
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+    
+    var tdImc = paciente.querySelector(".info-imc");
+    
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+    
+    if (peso <= 0 || peso > 1000) {
+        console.log("Peso inválido");
+        tdImc.textContent = "Peso Inválido!";
+        pesoEhValido = false;
+        // paciente.style.backgroundColor = "lightcoral"; // 'style' modifica o estilo do css
+        paciente.classList.add("paciente-invalido") // classList adiciona uma classe ao elemento, importante pois pode ser alterado no css
+    }
+    
+    if (altura <= 0 || altura > 3) {
+        console.log("Altura inválida")
+        tdImc.textContent = "Altura Inválida!";
+        alturaEhValida = false;
+        // paciente.style.backgroundColor = "lightcoral"; // 'style' modifica o estilo do css
+        paciente.classList.add("paciente-invalido") // classList adiciona uma classe ao elemento, importante pois pode ser alterado no css
+    }
+    
+    if (alturaEhValida && pesoEhValido) {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2); // to.Fixed escolhe quantas casas decimais deseja no código
+    }
 }
 
-if (altura <= 0 || altura > 3) {
-    console.log("Altura inválida")
-    tdImc.textContent = "Altura Inválida!";
-    alturaEhValida = false;
-}
-
-if (alturaEhValida && pesoEhValido) {
-    var imc = peso / (altura * altura);
-    tdImc.textContent = imc;
-}
 
 
